6ea50e7ff262c89cd5c59907be0efdbbd58253f8
Bruno Bousquet <21108660+brunob45@users.noreply.github.com>
2023-12-19 12:11:58 -0500

    fix update script

--- extra/update.sh
+++ extra/update.sh
@@ -16,35 +16,35 @@ fi
 
 echo "Replacing files"
 rm -r src/os/*
-cp -r extra/chibios/rt src/os/rt
-cp -r extra/chibios/oslib src/os/oslib
-cp -r extra/chibios/license src/os/license
+cp -r extra/chibios/os/rt src/os/rt
+cp -r extra/chibios/os/oslib src/os/oslib
+cp -r extra/chibios/os/license src/os/license
 cp -r extra/cmsis/CMSIS/Core/Include/ src/os/CMSIS
 
 mkdir src/os/hal
-cp extra/chibios/hal/include/hal_st.h src/os/hal/
-cp extra/chibios/hal/osal/rt-nil/osal.h src/os/hal/
-cp extra/chibios/hal/src/hal_st.c src/os/hal/
+cp extra/chibios/os/hal/include/hal_st.h src/os/hal/
+cp extra/chibios/os/hal/osal/rt-nil/osal.h src/os/hal/
+cp extra/chibios/os/hal/src/hal_st.c src/os/hal/
 
 mkdir src/os/avr
-cp extra/chibios/common/ports/AVR/chcore.c src/os/avr/
-cp extra/chibios/common/ports/AVR/chcore.h src/os/avr/
-cp extra/chibios/common/ports/AVR/compilers/GCC/chtypes.h src/os/avr/
-cp extra/chibios/hal/osal/rt-nil/chcore_timer.h src/os/avr/
-cp extra/chibios/common/portability/GCC/ccportab.h src/os/avr/
+cp extra/chibios/os/common/ports/AVR/chcore.c src/os/avr/
+cp extra/chibios/os/common/ports/AVR/chcore.h src/os/avr/
+cp extra/chibios/os/common/ports/AVR/compilers/GCC/chtypes.h src/os/avr/
+cp extra/chibios/os/hal/osal/rt-nil/chcore_timer.h src/os/avr/
+cp extra/chibios/os/common/portability/GCC/ccportab.h src/os/avr/
 
 mkdir src/os/arm
-cp extra/chibios/common/ports/ARMv7-M/mpu.h src/os/arm/
-cp extra/chibios/common/ports/ARM-common/chtypes.h src/os/arm/
-cp extra/chibios/hal/osal/rt-nil/chcore_timer.h src/os/arm/
-cp extra/chibios/common/portability/GCC/ccportab.h src/os/arm/
+cp extra/chibios/os/common/ports/ARMv7-M/mpu.h src/os/arm/
+cp extra/chibios/os/common/ports/ARM-common/chtypes.h src/os/arm/
+cp extra/chibios/os/hal/osal/rt-nil/chcore_timer.h src/os/arm/
+cp extra/chibios/os/common/portability/GCC/ccportab.h src/os/arm/
 
-cp extra/chibios/common/ports/ARMv6-M/chcore.c src/os/arm/chcore_v6m.c
-cp extra/chibios/common/ports/ARMv6-M/chcore.h src/os/arm/chcore_v6m.h
-cp extra/chibios/common/ports/ARMv7-M/chcore.c src/os/arm/chcore_v7m.c
-cp extra/chibios/common/ports/ARMv7-M/chcore.h src/os/arm/chcore_v7m.h
-cp extra/chibios/common/ports/ARMv6-M/compilers/GCC/chcoreasm.S src/os/arm/chcoreasm_v6m.S
-cp extra/chibios/common/ports/ARMv7-M/compilers/GCC/chcoreasm.S src/os/arm/chcoreasm_v7m.S
+cp extra/chibios/os/common/ports/ARMv6-M/chcore.c src/os/arm/chcore_v6m.c
+cp extra/chibios/os/common/ports/ARMv6-M/chcore.h src/os/arm/chcore_v6m.h
+cp extra/chibios/os/common/ports/ARMv7-M/chcore.c src/os/arm/chcore_v7m.c
+cp extra/chibios/os/common/ports/ARMv7-M/chcore.h src/os/arm/chcore_v7m.h
+cp extra/chibios/os/common/ports/ARMv6-M/compilers/GCC/chcoreasm.S src/os/arm/chcoreasm_v6m.S
+cp extra/chibios/os/common/ports/ARMv7-M/compilers/GCC/chcoreasm.S src/os/arm/chcoreasm_v7m.S
 
 # Patch ports
 echo "Applying patches"
